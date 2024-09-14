e357c7b152e0b9e64c76d0e77a40ba0d4ab3f7c5
Ziyatsi Fanye Rocha Guerrero <ziyatsir@gmail.com>
2024-09-14 12:18:50 -0600

    cuarto commit, trabajo final 1

--- src/temas-ts/09-areas.ts
+++ src/temas-ts/09-areas.ts
@@ -1,4 +1,4 @@
-export class Area{
+/*export class Area{
    protected areaRec(base: number, altura:number):number{
         return base * altura;
     }
@@ -13,4 +13,68 @@ export class Area{
 const area = new Area();
 console.log(`el area del rectangulo es: ${area.areaRec(2,13)}`);
 console.log(`el area del cuadrado es: ${area.areaCuadrado(20)}`);
-console.log(`el area del circulo es: ${area.areaCir(5)}`);
+console.log(`el area del circulo es: ${area.areaCir(5)}`); 
+FALTAN LOS CONSTRUCTURES E IMPRIMIR, CON UNOS PROTECTED*/
+// Clase para calcular el área de diferentes figuras
+
+/*export class Figuras{
+   
+    constructor(private tipo: string, private area: number, private cal?: number) {
+        //super();//lleva un super el constructor porque asi va la estructura 
+    }
+
+    calcularArea(): number {
+        switch (this.tipo) {
+            case 'rectangulo':
+                return this.area * (this.cal ?? 0); 
+                //aqui si la area2 es 0 o no tiene valor se ponen los ??
+                //porque son valores nulos 
+            case 'cuadrado':
+                return this.area * this.area;
+            case 'circulo':
+                return Math.PI * this.area * this.area;
+            default:
+                return 0;
+        }
+    }
+}
+*/
+
+export class FigurasA{
+    protected base:number;
+    protected altura:number; //solo se separan los protected, primero es el pro, despues la funcion de cada figura 
+    protected radio:number;
+    protected lado:number;
+
+    constructor(base:number, altura:number, lado:number, radio:number) {
+        this.base = base;
+        this.altura = altura;
+        this.radio = radio;
+        this.lado = lado;
+    }
+
+    areaRectangulo(){
+        let areaR; //se declara
+        areaR = this.base * this.altura; //aqui se hace el calculo usando los protected
+        return areaR;
+    }
+    areaCirculo(){
+        let areaC; //se declara 
+        areaC = Math.PI * (this.radio * this.radio); //aqui se hace el calculo usando los protected
+        return areaC;
+    }
+    areaCuadrado(){
+        let areaC2; 
+        areaC2 = this.lado * this.lado; //aqui se hace el calculo usando los protected
+        return areaC2; //por eso se manda a regresar con el return 
+    }
+}
+
+
+
+
+
+
+
+
+
--- src/temas-ts/10-volumen.ts
+++ src/temas-ts/10-volumen.ts
@@ -1,6 +1,6 @@
 
 
-import {Area} from "./09-areas.ts";
+/*import {Area} from "./09-areas.ts";
 
 class Volumen extends Area{
     volumenPris(base:number,altura:number, profundidad:number): number{
@@ -19,4 +19,53 @@ const volumen = new Volumen();
 console.log(`volumen del prisma: ${volumen.volumenPris(4,10,4)}`);
 console.log(`volumen del cilindro: ${volumen.volumenCili(3,5)}`);
 console.log(`volumen de la piramide: ${volumen.volumenPira(4,7,10)}`);
+AQUI SE HACE LA HERENCIA PERO FALTAN LOS CONSTRUCTORES */
+
+import {FigurasA} from "./09-areas.ts";
+class Volumen extends FigurasA{
+    private altura2:number;
+   constructor(base:number, altura:number, lado:number, radio:number, altura2:number){
+    super(base, altura, lado, radio); //aqui se pasan los valores de la herencia 
+    this.altura2=altura2;
+     //solo se ponen los parametros ya que se estan 
+    //poniendo primero en el constructor
+   }
+   volumenP():void{
+    let volPr = this.areaRectangulo() * this.altura2;
+    console.log(`area del prisma: ${volPr}`)
+   }
+   volumenPira():void{
+    let volPi = this.areaCuadrado() * this.altura2;
+    console.log(`area de la piramide: ${volPi}`)
+   }
+   volumenCili():void{
+    let volCil = this.areaCirculo() * this.altura2;
+    console.log(`area de la piramide: ${volCil}`)
+   }
+
+}
+
+console.log("areas");
+const area = new FigurasA(4,7,8,10);
+console.log(`area del rectangulo: ${area.areaRectangulo()}`);
+console.log(`area del cuadrado: ${area.areaCuadrado()}`);
+console.log(`area del circulo: ${area.areaCirculo()}`);
+
+
+
+const volumenes = new Volumen (10,8,5,1,7);
+volumenes.volumenPira();
+volumenes.volumenP();
+volumenes.volumenCili();
+/*const prisma = new Figuras2('prisma', rectangulo, 10);
+console.log(`Volumen del prisma: ${volumen.areaRectangulo()}`);
+
+const piramide = new Figuras2('piramide', cuadrado, 6);
+console.log(`Volumen de la piramide: ${piramide.calcularVolumen()}`);
+
+const cilindro = new Figuras2('cilindro', circulo, 7);
+console.log(`Volumen del cilindro: ${cilindro.calcularVolumen()}`);
+
+*/
+
 
